d39c0586c6c7b4ad817ef724f57578257731f9ad
iamz-tao <phiyada.s@ku.th>
2020-03-01 14:31:26 +0700

    Edit style register page

--- src/navigate/Register.js
+++ src/navigate/Register.js
@@ -1,155 +1,255 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Avatar, ButtonGroup} from 'react-native-elements';
 import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    StatusBar,
-    TextInput,
-    Button,
-    Alert,
-    Image,
-    TouchableHighlight
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  Image,
+  TouchableHighlight,
 } from 'react-native';
 
 export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      role: '',
+      firstname: '',
+      lastname: '',
+      mobile: '',
+      email: '',
+      password: '',
+      selectedIndex: 0,
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            id: '',
-            role: '',
-            firstname: '',
-            lastname: '',
-            mobile: '',
-            email: '',
-            password: ''
-        }
+  updateIndex = selectedIndex => {
+    if (selectedIndex === 1) {
+      this.setState({
+        role: 'STUDENT',
+        selectedIndex,
+      });
+    } else {
+      this.setState({
+        role: 'PROFESSOR',
+        selectedIndex,
+      });
     }
+  };
 
-    Event_Register = async () => {
-        const response = await fetch('https://us-central1-kpscheckin.cloudfunctions.net/api/register', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                id: this.state.id,
-                firstname: this.state.firstname,
-                lastname: this.state.lastname,
-                email: this.state.email,
-                password: this.state.password,
-                role:this.state.role,
-                mobile:this.state.mobile
-            }),
-        })
-        const responseJson = await response.json()
-        if(responseJson.Error === undefined){
-            Alert.alert("ADD Success")
-            this.props.navigation.navigate('Home')
-        }
-        else{
-            Alert.alert(`${responseJson.Error}`)
-        }
-         
+  Event_Register = async () => {
+    const response = await fetch(
+      'https://us-central1-kpscheckin.cloudfunctions.net/api/register',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: this.state.id,
+          firstname: this.state.firstname,
+          lastname: this.state.lastname,
+          email: this.state.email,
+          password: this.state.password,
+          role: this.state.role,
+          mobile: this.state.mobile,
+        }),
+      },
+    );
+    const responseJson = await response.json();
+    if (responseJson.Error === undefined) {
+      Alert.alert('ADD Success');
+      this.props.navigation.navigate('Home');
+    } else {
+      Alert.alert(`${responseJson.Error}`);
     }
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        placeholder="STUDENT ID / PROFESSOR ID"
-                        onChangeText={(id) => this.setState({ id })}
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.inputs}
-                        placeholder="Nisit/Professor"
-                        onChangeText={(role) => this.setState({ role })}
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.inputs}
-                        placeholder="firstname"
-                        onChangeText={(firstname) => this.setState({ firstname })}
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.inputs}
-                        placeholder="lastname"
-                        onChangeText={(lastname) => this.setState({ lastname })}
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.inputs}
-                        placeholder="mobile"
-                        onChangeText={(mobile) => this.setState({ mobile })}
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.inputs}
-                        placeholder="E-Mail"
-                        onChangeText={(email) => this.setState({ email })}
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput style={styles.inputs}
-                        placeholder="PASSWORD"
-                        onChangeText={(password) => this.setState({ password })}
-                    />
-                </View>
-                <TouchableHighlight style={styles.buttonContainer} onPress={this.Event_Register}>
-                    <Text>SUBMIT</Text>
-                </TouchableHighlight>
+  };
+  render() {
+    const user_type = ['LECTURER', 'STUDENT'];
+    const {selectedIndex} = this.state;
+
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.containerWrapper}>
+            <Text style={styles.styleHeader}>REGISTER</Text>
+            <Avatar
+              rounded
+              size="large"
+              source={{
+                uri:
+                  'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+              }}
+              showEditButton
+            />
+          </View>
+          <View style={styles.styleInputWrapper}>
+            <View style={styles.inputContainer}>
+              <Text style={styles.styleLabel}>STUDENT ID / LECTURER ID :</Text>
+              <TextInput
+                style={styles.inputs}
+                placeholder="Student ID / Lecturer ID"
+                onChangeText={id => this.setState({id})}
+              />
             </View>
-        );
-    }
+            <View style={styles.inputContainer}>
+              <Text style={styles.styleLabel}>USER TYPE :</Text>
+              <ButtonGroup
+                containerStyle={{
+                  borderColor: 'rgba(36, 52, 69, 0.5)',
+                  borderRadius: 21,
+                  height: 45,
+                }}
+                selectedButtonStyle={{ 
+                    backgroundColor: '#CA5353',
+                }}
+                onPress={this.updateIndex}
+                selectedIndex={selectedIndex}
+                buttons={user_type}
+              />
+            </View>
+            <View style={styles.inputContainerGroup}>
+            <View style={{ paddingRight: 16, flex: 1}}>
+              <Text style={styles.styleLabel}>FIRSTNAME :</Text>
+              <TextInput
+                style={styles.inputs}
+                placeholder="Firstname"
+                onChangeText={firstname => this.setState({firstname})}
+              />
+            </View>
+            <View style={{ flex: 1}}>
+              <Text style={styles.styleLabel}>LASTNAME :</Text>
+              <TextInput
+                style={styles.inputs}
+                placeholder="Lastname"
+                onChangeText={lastname => this.setState({lastname})}
+              />
+            </View>
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={styles.styleLabel}>PHONE NUMBER :</Text>
+              <TextInput
+                style={styles.inputs}
+                placeholder="Phone Number"
+                onChangeText={mobile => this.setState({mobile})}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={styles.styleLabel}>EMAIL :</Text>
+              <TextInput
+                style={styles.inputs}
+                placeholder="Email"
+                onChangeText={email => this.setState({email})}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={styles.styleLabel}>PASSWORD :</Text>
+              <TextInput
+                style={styles.inputs}
+                placeholder="Password"
+                onChangeText={password => this.setState({password})}
+              />
+            </View>
+            <TouchableHighlight
+              style={styles.buttonContainer}
+              onPress={this.Event_Register}>
+              <Text style={{ color: '#ffffff'}}>SUBMIT</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#DCDCDC',
-    },
-    inputContainer: {
-        borderBottomColor: '#F5FCFF',
-        backgroundColor: '#FFFFFF',
-        borderRadius: 30,
-        borderBottomWidth: 1,
-        width: 250,
-        height: 45,
-        marginBottom: 20,
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    inputs: {
-        height: 45,
-        marginLeft: 16,
-        borderBottomColor: '#FFFFFF',
-        flex: 1,
-    },
-    inputIcon: {
-        width: 30,
-        height: 30,
-        marginLeft: 15,
-        justifyContent: 'center'
-    },
-    buttonContainer: {
-        height: 45,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20,
-        width: 250,
-        borderRadius: 30,
-    },
-    loginButton: {
-        backgroundColor: "#00b5ec",
-    },
-    loginText: {
-        color: 'white',
-    }
+  container: {
+    position: 'relative',
+    display: 'flex',
+    fontFamily: 'Kanit',
+    padding: 14,
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  containerWrapper: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    paddingRight: 8,
+  },
+  styleHeader: {
+    display: 'flex',
+    flex: 1,
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 28,
+    lineHeight: 42,
+    paddingLeft: 16,
+  },
+  styleLabel: {
+    fontSize: 14,
+    lineHeight: 21,
+    display: 'flex',
+    paddingLeft: 12,
+  },
+  styleInputWrapper: {
+    display: 'flex',
+    marginTop: 16,
+  },
+  inputContainer: {
+    position: 'relative',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 16,
+    width: 300,
+    marginBottom: 12,
+    flexDirection: 'column',
+    display: 'flex',
+  },
+  inputs: {
+    height: 45,
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'rgba(36, 52, 69, 0.5)',
+    borderRadius: 21,
+    paddingLeft: 12,
+  },
+  inputContainerGroup: {
+    position: 'relative',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 16,
+    width: 300,
+    marginBottom: 12,
+    flexDirection: 'row',
+    display: 'flex',
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    display: 'flex',
+    height: 45,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    borderWidth: 1,
+    backgroundColor: '#CA5353',
+    borderColor: '#CA5353',
+    color: '#ffffff',
+  },
+  styleButtonGroup: {
+    borderRadius: 21,
+    borderColor: '#CA5353',
+  },
 });
